5125da72af5e336ed8ffa9088a427accc64d1bbc
MarcelKaemper <marcelkaemper862@gmail.com>
2018-07-31 23:15:34 +0200

    Revert

--- game/adminArea.php
+++ game/adminArea.php
@@ -1,22 +1,33 @@
 <style>
-    div{
+    #btn{
         height: 100px;
         width: 100px;
         border-radius: 50px;
         background: radial-gradient(circle, red, orange);
         margin: auto;
-        box-shadow: 0 9px #999;
+        float: right;
+        box-shadow: 0 9px #666;
     }
-    div:active{
+    #btn:active{
         background-color: #3e8e41;
-        box-shadow: 0 5px #666;
+        box-shadow: 0 2px #666;
         transform: translateY(4px);
+    }
+    #container{
+        border: 1px solid #000;
+        display: inline-block;
+        padding: 5px 10px;
     }
 </style>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+<script>
+    $(document).ready(function(){
+        $("#btn").click(function(){
+            location.reload();
+        });
+    });
+</script>
 <?php
-    echo "<b><u>Registered users:<br></b></u>";
     include("../database.php");
     // Create connection
     $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
@@ -29,16 +40,17 @@
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
+        echo "<div id='container'>";
+        echo "<b><u>Registered users:<br></b></u>";
         // output data of each row
         while($row = $result->fetch_assoc()) {
             echo "Username: " . $row["username"]. "<br>";
         }
+        echo "</div>";
     } else {
         echo "0 results";
     }
     $conn->close();
 ?>
 
-<label id="onlineUsers"></label>
-
-<div></div>
+<div id="btn"></div>
