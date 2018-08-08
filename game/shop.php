1ccf5235d19a9b6d0cd5590d806d31bc4ba4caf5
MarcelKaemper <marcelkaemper862@gmail.com>
2018-08-09 01:27:25 +0200

    Added shop

--- game/shop.php
+++ game/shop.php
@@ -1,5 +1,13 @@
 <?php
 
-?>
+$sql = "SELECT id, name, start_price, price_level_mul, description FROM shop;";
 
-Shop
+if(!$db_erg = mysqli_query($db_link, $sql)) {
+    echo "<p>Error!</p>";
+}
+
+while($data = mysqli_fetch_array($db_erg, MYSQLI_ASSOC)) {
+    echo "<div style='border: 1px solid black; padding: 5px' title=".$data["description"]."> Name: ".$data["name"]."</div><br>";
+}
+
+?>
