5446837b8c6862754b6e13e0583f6bc87e6f852f
MarcelKaemper <marcelkaemper862@gmail.com>
2018-08-10 00:21:18 +0200

    Changed style of shop items | reads item levels for each user

--- game/shop.php
+++ game/shop.php
@@ -1,13 +1,22 @@
 <?php
 
 $sql = "SELECT id, name, start_price, price_level_mul, description FROM shop;";
+$sql2 = "SELECT item_level FROM items WHERE uuid='".$_SESSION['uuid']."';";
 
 if(!$db_erg = mysqli_query($db_link, $sql)) {
     echo "<p>Error!</p>";
 }
 
 while($data = mysqli_fetch_array($db_erg, MYSQLI_ASSOC)) {
-    echo "<div style='border: 1px solid black; padding: 5px' title=".$data["description"]."> Name: ".$data["name"]."</div><br>";
+    echo "<div style='border: 1px solid black; padding: 5px; margin: 5px;display: inline-block' title=".$data["description"]."> Name: ".$data["name"]."</div><br>";
 }
+
+if(!$db_erg2 = mysqli_query($db_link, $sql2)) {
+    echo "<p>Error!</p>";
+}
+
+// while($data2 = mysqli_fetch_array($db_erg2, MYSQLI_ASSOC)) {
+//     echo "<div style='border: 1px solid black; padding: 5px; margin: 5px;display: inline-block'>".$data2["item_level"]."</div><br>";
+// }
 
 ?>
